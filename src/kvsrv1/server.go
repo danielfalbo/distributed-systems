1542e47486472e26814529f3cfba9ac8bf9a5c7f
Daniel <39460524+danielfalbo@users.noreply.github.com>
2025-12-23 20:40:09 +0100

    [kv] implement simplest kv server

--- src/kvsrv1/server.go
+++ src/kvsrv1/server.go
@@ -18,23 +18,36 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
+type KVState struct {
+	Value 	string
+	Version rpc.Tversion
+}
 
 type KVServer struct {
 	mu sync.Mutex
-
-	// Your definitions here.
+	data map[string]*KVState
 }
 
 func MakeKVServer() *KVServer {
 	kv := &KVServer{}
-	// Your code here.
+	kv.data = make(map[string]*KVState)
 	return kv
 }
 
 // Get returns the value and version for args.Key, if args.Key
 // exists. Otherwise, Get returns ErrNoKey.
 func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	state, exists := kv.data[args.Key]
+	if exists {
+		reply.Value = state.Value
+		reply.Version = state.Version
+		reply.Err = rpc.OK
+	} else {
+		reply.Err = rpc.ErrNoKey
+	}
 }
 
 // Update the value for a key if args.Version matches the version of
@@ -42,13 +55,35 @@ func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 // If the key doesn't exist, Put installs the value if the
 // args.Version is 0, and returns ErrNoKey otherwise.
 func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	state, exists := kv.data[args.Key]
+	if exists {
+		if state.Version == args.Version {
+			state.Value = args.Value
+			state.Version++
+			kv.data[args.Key] = state
+			reply.Err = rpc.OK
+		} else {
+			reply.Err = rpc.ErrVersion
+		}
+	} else {
+		if args.Version == 0 {
+			kv.data[args.Key] = &KVState{
+				Value: 		args.Value,
+				Version: 	1,
+			}
+			reply.Err = rpc.OK
+		} else {
+			reply.Err = rpc.ErrNoKey
+		}
+	}
 }
 
 // You can ignore Kill() for this lab
 func (kv *KVServer) Kill() {
 }
-
 
 // You can ignore all arguments; they are for replicated KVservers
 func StartKVServer(ends []*labrpc.ClientEnd, gid tester.Tgid, srv int, persister *tester.Persister) []tester.IService {
