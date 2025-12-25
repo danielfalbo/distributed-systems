610a38e182fd3f5c0b023810d7ebf4639b80a170
Daniel <39460524+danielfalbo@users.noreply.github.com>
2025-12-25 10:34:51 +0100

    [kv lock] be resilient under unstable network

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -3,7 +3,6 @@ package lock
 import (
 	"6.5840/kvtest1"
 	"6.5840/kvsrv1/rpc"
-	"time"
 )
 
 // The lock state will be:
@@ -42,35 +41,49 @@ type Lock struct {
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
 	// Init lock with given ck, given l, new client ID, and latest free version 0.
 	lk := &Lock{ck: ck, l: l, id: kvtest.RandValue(8), v: rpc.Tversion(0)}
-
-	// Attempt lock inizialization in case we're the first client to ever connect.
-	// It's ok for this to fail: it just means we're not the first client.
-	lk.ck.Put(lk.l, Free, lk.v)
-
 	return lk
 }
 
 func (lk *Lock) Acquire() {
-	// Keep trying to acquire the lock until we suceed.
+	// Keep trying to acquire the lock until we succeed.
 	// Use lk.l as key, out client ID lk.id as value,
 	// and lk.v as version. Recall that lk.v is our copy
 	// of the most recent version where we observed the kv
 	// to be free or owned by us.
-	for lk.ck.Put(lk.l, lk.id, lk.v) != rpc.OK {
-		// If we are here, the Put failed,
-		// the last free version observed must be obsolete.
-		// Let's ping the kv until the version changes.
-		// Once it's free, we can update 'lk.v'.
-		value, version, err := Busy, lk.v, rpc.Err(rpc.ErrMaybe)
-		for err != rpc.OK || value != Free {
-			value, version, err = lk.ck.Get(lk.l)
+	err := rpc.Err("")
+	for err != rpc.OK {
+		err = lk.ck.Put(lk.l, lk.id, lk.v)
 
-			// No need to keep CPU and KV server busy checking all the time.
-			time.Sleep(10 * time.Millisecond)
+		// If we get ErrMaybe then we may have acquired it and waiting for it to
+		// become Free could deadlock. Therefore, we want to be sure: we want to
+		// disambiguate any ErrMaybe into either OK or ErrVersion.
+		if err == rpc.ErrMaybe {
+			value, _, _ := lk.ck.Get(lk.l)
+
+			if value == lk.id {
+				// If we are holding the lock, then we can go ahead just as if the Put
+				// succeeded, because it did.
+				err = rpc.OK
+			} else {
+				// Otherwise, we can go ahead as if the Put failed because of outdated
+				// version, because it did.
+				err = rpc.ErrVersion
+			}
 		}
 
-		// At this point it must be that err is OK and value is Free.
-		lk.v = version
+		// Inside the following block either the Put failed with ErrVersion or we
+		// got ErrMaybe and we disambiguated it to actually be an ErrVersion.
+		// Either way, the last free observed version must be obsolete. Let's ping
+		// the kv until we successfully get a reply and observe it to be Free.
+		if err == rpc.ErrVersion { // if err == rpc.ErrVersion || err == rpc.ErrMaybe
+			value, version, _ := Busy, lk.v, rpc.Err("")
+			for value != Free {
+				value, version, _ = lk.ck.Get(lk.l)
+			}
+
+			// At this point it must be that value is Free.
+			lk.v = version
+		}
 	}
 
 	// At this point we must have succeeded at acquiring the lock
@@ -80,5 +93,13 @@ func (lk *Lock) Acquire() {
 }
 
 func (lk *Lock) Release() {
-	lk.ck.Put(lk.l, Free, lk.v)
+	// In releasing the lock, we can afford an ErrMaybe:
+	// 	- if we get rpc.OK it obviously means we succeeded
+	// 	- if we get rpc.ErrMaybe it means our RPC must have been executed
+	// 		at most once, because we held the lock and now our version is
+	// 		out of sync with the kv server
+	err := rpc.Err("")
+	for err != rpc.OK  && err != rpc.ErrMaybe {
+		err = lk.ck.Put(lk.l, Free, lk.v)
+	}
 }
