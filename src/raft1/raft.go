3727b176f6d54132d552d5dfbe3eb73c97ee795a
Daniel <39460524+danielfalbo@users.noreply.github.com>
2026-01-02 21:47:07 +0100

    [raft] implement persistence

--- src/raft1/raft.go
+++ src/raft1/raft.go
@@ -1,13 +1,13 @@
 package raft
 
 import (
-	//	"bytes"
+	"bytes"
 	"math/rand"
 	"sync"
 	"sync/atomic"
 	"time"
 
-	//	"6.5840/labgob"
+	"6.5840/labgob"
 	"6.5840/labrpc"
 	"6.5840/raftapi"
 	"6.5840/tester1"
@@ -125,21 +125,17 @@ func Make(peers []*labrpc.ClientEnd, me int,
 
 // Save Raft's persistent state to stable storage,
 // where it can later be retrieved after a crash and restart.
-//
-// See paper's Figure 2 for a description of what should be persistent.
-//
-// NOTE: Before you've implemented snapshots, you should pass nil as the
-// 		second argument to persister.Save(). After you've implemented snapshots,
-// 		pass the current snapshot (or nil if there's not yet a snapshot).
 func (rf *Raft) persist() {
-	// Your code here (3C).
-	// Example:
-	// w := new(bytes.Buffer)
-	// e := labgob.NewEncoder(w)
-	// e.Encode(rf.xxx)
-	// e.Encode(rf.yyy)
-	// raftstate := w.Bytes()
-	// rf.persister.Save(raftstate, nil)
+	w := new(bytes.Buffer)
+	e := labgob.NewEncoder(w)
+
+	e.Encode(rf.Term)
+	e.Encode(rf.Vote)
+	e.Encode(rf.log)
+
+	raftstate := w.Bytes()
+
+	rf.persister.Save(raftstate, nil)
 }
 
 // Restore previously persisted state.
@@ -147,19 +143,21 @@ func (rf *Raft) readPersist(data []byte) {
 	if data == nil || len(data) < 1 { // bootstrap without any state?
 		return
 	}
-	// Your code here (3C).
-	// Example:
-	// r := bytes.NewBuffer(data)
-	// d := labgob.NewDecoder(r)
-	// var xxx
-	// var yyy
-	// if d.Decode(&xxx) != nil ||
-	//    d.Decode(&yyy) != nil {
-	//   error...
-	// } else {
-	//   rf.xxx = xxx
-	//   rf.yyy = yyy
-	// }
+
+	r := bytes.NewBuffer(data)
+	d := labgob.NewDecoder(r)
+
+	var currentTerm int
+	var votedFor int
+	var log []LogEntry
+
+	d.Decode(&currentTerm)
+	d.Decode(&votedFor)
+	d.Decode(&log)
+
+	rf.Term = currentTerm
+	rf.Vote = votedFor
+	rf.log = log
 }
 
 /* =================== Raft API (raftapi/raftapi.go) ====================
@@ -466,6 +464,8 @@ func (rf *Raft) AppendEntries(args *AppendEntriesArgs,
 			rf.log = append(rf.log, logEntry)
 		}
 	}
+
+	rf.persist()
 
 	// If leaderCommit > commitIndex,
 	// set commitIndex = min(leaderCommit, index of last new entry).
